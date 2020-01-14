26b38a24a88c51e36bb281a0e0d86dca76f7807f
System Administrator <root@naraes-MacBook-Pro.local>
2020-01-14 12:34:40 +0900

    # BE7. create graphQL Types

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,11 @@
 import { GraphQLServer } from "graphql-yoga";
 import connection from "./ormConfig";
+import schema from "./schema";
 
-// 인자값과 리턴되는 값의 타입을 지정
-const typeDefs = `
-type Query{
-  sayHello: String!
-}
-`;
-// GraphQL : Query, Mutation, Subscribe
-const resolvers = {
-  Query: {
-    sayHello: () => "Hi there 🙂"
-  }
-};
-
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({ schema });
 
 connection.then(() =>
   server.start(() =>
     console.log("My First GraphQL Server is running on localhost:4000")
   )
 );
-
